d96c1ef9c41d88de4b784642614b84ce23b84ec1
马博渊 <793960628@qq.com>
2019-03-04 20:13:02 +0800

    3-4

--- ImageProcessing/Main.py
+++ ImageProcessing/Main.py
@@ -6,8 +6,10 @@ import cv2
 if __name__ == "__main__":
     project_address = os.getcwd()
     start_time = datetime.datetime.now()
-    fuse_method = "fadeInAndFadeOut"
-    tracker = ImageTrack(os.path.join(project_address, "random_images"))
+    # fuse_method = "multiBandBlending"
+    # fuse_method = "fadeInAndFadeOut"
+    fuse_method = "notFuse"
+    tracker = ImageTrack(os.path.join(project_address, "random_images"),fuse_method=fuse_method)
     tracked_image = tracker.start_track_and_fuse()
     end_time = datetime.datetime.now()
     print("The time of tracking is {}".format(end_time - start_time))
